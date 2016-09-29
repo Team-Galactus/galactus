394092216f7ccc7c24310e30df84db0ba94ce918
madmarks <bratmiro@gmail.com>
2016-09-29 08:27:04 +0300

    SPA skeleton created

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+﻿
+let router = new Navigo(null, true);
+
+let controllersInstance = controllers.get(dataService, templates);
+
+router
+    .on("login", controllersInstance.login)
+    .on("home", controllersInstance.home)
+    .on("homeworks", controllersInstance.homeworks)
+    .on("workshops", controllersInstance.workshops)
+    .on("exams", controllersInstance.exams)
+    .resolve(); // Very Important !!!
+
+//toggle navigation link active class
+$('#dashboardNav li').on('click', (event) => {
+    $("#dashboardNav li.active").removeClass("active");
+    $(event.currentTarget).addClass(" active");
+});

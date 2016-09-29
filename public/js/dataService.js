ac6aa37d24803fe4eebbe3ce3263f8eb6e852c6c
madmarks <bratmiro@gmail.com>
2016-09-29 13:39:40 +0300

    add homeworks dataService (temporarily hack before we find out how to use some real backend service)

--- public/js/dataService.js
+++ public/js/dataService.js
@@ -1,4 +1,25 @@
 ﻿
-var dataService = {
+let homeworks = 
+    [{
+        id: 1,
+        course: "JS-App",
+        deadline: "04.10.2016"
+    },
+    {
+        id: 2,
+        course: "HQC-2",
+        deadline: "07.10.2016"
+    }];
 
+
+var dataService = {
+    homeworks() {
+
+        // this is a temporarily hack
+        return Promise.resolve()
+            .then(() => {
+                return { homeworks };
+            }); 
+        //return requester.getJSON("/api/homeworks");
+    }
 };
